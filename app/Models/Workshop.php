87e46c7909bc8baac0ea990cf8e2d990f794c1eb
ruaa <roaanoman@gmail.com>
2025-09-10 17:58:36 +0300

    fix offerings relationship

--- app/Models/Workshop.php
+++ app/Models/Workshop.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Workshop extends Model
 {
@@ -31,7 +32,7 @@ class Workshop extends Model
         return $this->belongsTo(User::class, "created_by");
     }
 
-    public function offerings(): BelongsTo
+    public function offerings()
     {
         return $this->hasMany(WorkshopOffering::class, "workshop_id");
     }
